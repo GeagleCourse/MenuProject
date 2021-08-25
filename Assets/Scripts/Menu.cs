83b0505604b2caca9a99df3375e25e372df628d1
Haetham <halhaddad@geagle.tech>
2021-08-25 13:33:09 +0300

    Initial Commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame(int level)
+    {
+        Debug.Log("Start the game!");
+        SceneManager.LoadScene(level); 
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit the game!");
+        Application.Quit();
+    }
+
+
+}
